111b03394cca0e128871444b3c99061d4a51409b
Karl74 <carlos.castro74@gmail.com>
2017-05-20 05:30:33 -0500

    seinfeld app completed

--- handleBars/magicSchool/server.js
+++ handleBars/magicSchool/server.js
@@ -39,4 +39,40 @@ app.get("/cast", function(req, res){
   });
 });
 
+app.get("/coolness", function(req,res){
+    connection.query("SELECT * FROM actors ORDER BY coolness_points", function(err, result){
+      if(err){
+        throw err;
+      }
+      html = "<h1> Seinfeld  Cast</h1><h2> Order by Coolness</h2>";
+      html +="<ul>";
+      for(var i = 0; i < result.length; i ++ ){
+        html += "<li><p>"+result[i].id+"</p><p>"+result[i].name+"</p><p>"+result[i].coolness_points+"</p><p>"+result[i].attitude+"</p></li>";
+      }
+      html += "</ul>";
+      res.send(html);
+    });
+});
+
+app.get("/attitude", function(req, res){
+  var query = "SELECT * FROM actors ORDER BY attitude"
+  connection.query(query,function (err, result){
+    if(err){
+      throw err;
+    }
+    html = "<h1> Seinfeld  Cast</h1><h2> Order by Attitude</h2>";
+    html +="<ul>";
+    for(var i = 0; i < result.length; i ++ ){
+      html += "<li><p>"+result[i].id+"</p><p>"+result[i].name+"</p><p>"+result[i].coolness_points+"</p><p>"+result[i].attitude+"</p></li>";
+    }
+    html += "</ul>";
+    res.send(html);
+  });
+})
+
+
+
+
+
+
 app.listen(PORT);
